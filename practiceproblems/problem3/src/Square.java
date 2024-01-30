57f3c5f5232318d6b0b76b9eed91897fea26c8ed
markharshbarger <markharshbarger25@gmail.com>
2024-01-30 10:52:34 -0500

    completed class setup

---
+++ practiceproblems/problem3/src/Square.java
@@ -0,0 +1,23 @@
+public class Square {
+    private double side;
+
+    public Square(double side) {
+        if (side < 0) {
+            throw new IllegalSideLengthException();
+        }
+        this.side = side;
+    }
+
+    @Override
+    public String toString() {
+        return "side = " + side;
+    }
+
+    public double getPerimeter() {
+        return side * 4.0;
+    }
+
+    public double getArea() {
+        return side * side;
+    }
+}
