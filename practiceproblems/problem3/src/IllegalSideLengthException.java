3da21acf284f3cbd8f6bf996f965df7f8ea0e7aa
markharshbarger <markharshbarger25@gmail.com>
2024-01-30 19:10:13 -0500

    added JavaDoc

--- practiceproblems/problem3/src/IllegalSideLengthException.java
+++ practiceproblems/problem3/src/IllegalSideLengthException.java
@@ -1,4 +1,9 @@
 public class IllegalSideLengthException extends IllegalArgumentException {
+    /**
+     * Constructor for IllegalSideLengthException that has a custom message and uses super class constructor
+     * 
+     * @param message - (String) that has a custom message based on the error
+     */
     public IllegalSideLengthException(String message) {
         super(message);
     }
--- practiceproblems/problem3/src/Square.java
+++ practiceproblems/problem3/src/Square.java
@@ -1,6 +1,13 @@
+
+// Square class represents a square object that has 4 sides of length "side" of type (double).
+// Methods to get perimeter and area are included, along with a toString method
 public class Square {
     private double side;
 
+    /**
+     * Constructor for Square class, sets side with a double.
+     * @param side - (double) if double is less than 0, an IllegalSideLengthException is thrown
+     */
     public Square(double side) {
         if (side < 0) {
             throw new IllegalSideLengthException("Negative length: " + side);
@@ -8,15 +15,31 @@ public class Square {
         this.side = side;
     }
 
+    /** 
+     * toString method that overrides the default
+     * 
+     * @return the string "side = " + the square side length
+     * 
+    */
     @Override
     public String toString() {
         return "side = " + side;
     }
 
+    /**
+     * Gets perimeter of the square
+     * 
+     * @return - (double) side * 4.0; which is perimeter for a squarex
+     */
     public double getPerimeter() {
         return side * 4.0;
     }
 
+    /**
+     * Gets area of the square
+     * 
+     * @return - (double) side * side; which is area for a square
+     */
     public double getArea() {
         return side * side;
     }
