5cb71c56f6fe70480056956f6bd9e45606d7d881
markharshbarger <markharshbarger25@gmail.com>
2024-01-30 23:03:52 -0500

    added code for methods

--- projects/project1/src/Item.java
+++ projects/project1/src/Item.java
@@ -5,32 +5,38 @@ public class Item {
     private boolean included;
     
     public Item(String name, double weight, int value) {
-
+        this.name = name;
+        this.weight = weight;
+        this.value = value;
+        included = false;
     }
 
     public Item(Item other) {
-
+        name = other.name;
+        weight = other.weight;
+        value = other.value;
+        included = other.included;
     }
 
     public double getWeight() {
-
+        return weight;
     }
 
-    public int getValuie() {
-
+    public int getValue() {
+        return value;
     }
 
     public boolean isIncluded() {
-
+        return included;
     }
 
     public void setIncluded(boolean included) {
-
+        this.included = included;
     }
 
     @Override
     public String toString() {
-
+        return name + "(" + weight + " lbs, $" + value + ")";
     }
 }
 
