ee87e4bbb48bbfc0d062b5e3db720e45ff690e8e
markharshbarger <markharshbarger25@gmail.com>
2024-02-13 06:04:53 -0500

    added author name and email for wsu

--- practiceproblems/problem5/src/App.java
+++ practiceproblems/problem5/src/App.java
@@ -1,5 +1,7 @@
 
 public class App {
+    // Name: Mark Harshbarger
+    // WSU email: harshbarger.26@wright.edu
     public static void main(String[] args) throws Exception {
         new MyFrame();
     }
